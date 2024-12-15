15a55ef4697fc171d58eadd4cd88f43f12c77413
Bahrul Ulum <deganteng1995@gmail.com>
2024-12-15 14:56:41 +0700

    up old

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-// import { util } from './util.js';
+import { util } from './util.js';
 import { like } from './like.js';
 import { guest } from './guest.js';
 import { theme } from './theme.js';
 import { audio } from './audio.js';
 import { offline } from './offline.js';
-// import { comment } from './comment.js';
+import { comment } from './comment.js';
 import { progress } from './progress.js';
 import { pagination } from './pagination.js';
 
@@ -18,10 +18,10 @@ document.addEventListener('DOMContentLoaded', () => {
     window.AOS.init();
 
     window.like = like;
-    // window.util = util;
+    window.util = util;
     window.guest = guest;
     window.theme = theme;
     window.audio = audio;
-    // window.comment = comment;
+    window.comment = comment;
     window.pagination = pagination;
 });
